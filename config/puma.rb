400adff64a786e5a4255c022f4ef4c3815762f79
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2023-06-20 14:29:07 +0600

    Render adopt

--- config/puma.rb
+++ config/puma.rb
@@ -18,8 +18,12 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+
+if ENV['RENDER'].present?
+  environment 'production'
+else
+  environment ENV.fetch("RAILS_ENV") { "development" }
+end
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
